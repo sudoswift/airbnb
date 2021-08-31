f8d9952d010f18e16c273dc7fb57b8474090a20c
sudoswift <sudoswift@gmail.com>
2021-08-31 22:28:28 +0900

    update settings.py installed apps

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 app_name = 'users'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path()
 ]
